4657e918d17eeafb36c0d2d4b267752515a848ff
Mohammed Hasaballah <mhasaballah21@gmail.com>
2023-05-22 01:02:52 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/mohammed_hasaballah_4_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mohammed_hasaballah_4_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:mohammed_hasaballah_4_01/listen_now.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter Demo',
+    home: ListenNow(),
+  ));
+}
